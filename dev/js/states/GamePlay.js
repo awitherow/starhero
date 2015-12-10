c140d4a8a04399b0976ea265c77874baa139bb95
Austin <au.witherow@gmail.com>
2015-12-10 13:29:11 +0100

    reorganized assets folder

--- dev/js/states/GamePlay.js
+++ dev/js/states/GamePlay.js
@@ -18,24 +18,26 @@ define(
 
         GamePlay.prototype.preload = function () {
             // terrain
-            this.game.load.image('background', 'assets/background.png');
-            this.game.load.image('ground', 'assets/platform.png');
-            this.game.load.image('ledge', 'assets/ledge-festive.png');
-            this.game.load.image('ledge-wide', 'assets/ledge-wide-festive.png');
-            this.game.load.image('ground-festive', 'assets/ground-wide-festive.png');
+            this.game.load.image('background', 'assets/env/background.png');
+            this.game.load.image('ground', 'assets/env/platform.png');
+            this.game.load.image('ledge', 'assets/env/ledge-festive.png');
+            this.game.load.image('ledge-wide', 'assets/env/ledge-wide-festive.png');
+            this.game.load.image('ground-festive', 'assets/env/ground-wide-festive.png');
 
             // powerups
-            this.game.load.image('star', 'assets/star.png');
-            this.game.load.image('diamond', 'assets/diamond.png');
-            this.game.load.image('firstaid', 'assets/firstaid.png');
-            this.game.load.image('pie', 'assets/pie.png');
+            this.game.load.image('star', 'assets/items/star.png');
+            this.game.load.image('diamond', 'assets/items/diamond.png');
+            this.game.load.image('firstaid', 'assets/items/firstaid.png');
+            this.game.load.image('pie', 'assets/items/pie.png');
 
             // characters
-            this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-            this.game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
-            this.game.load.spritesheet('guy-festive', 'assets/guy-festive.png', 44, 44);
-            this.game.load.spritesheet('bullet', 'assets/bullet.png', 14, 8);
-            this.game.load.spritesheet('snowball', 'assets/snowball.png', 11, 11);
+            this.game.load.spritesheet('dude', 'assets/char/player/dude.png', 32, 48);
+            this.game.load.spritesheet('guy-festive', 'assets/char/player/guy-festive.png', 44, 44);
+            this.game.load.spritesheet('baddie', 'assets/char/baddies/baddie.png', 32, 32);
+
+            // weapons
+            this.game.load.spritesheet('bullet', 'assets/weapons/bullet.png', 14, 8);
+            this.game.load.spritesheet('snowball', 'assets/weapons/snowball.png', 11, 11);
         };
 
         GamePlay.prototype.create = function () {
